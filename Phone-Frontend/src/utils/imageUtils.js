0f4bf1bc242d961f7996d34f76a9b56c471abc78
Hanaviet <tranquocvieta4k51nl@gmail.com>
2025-12-31 01:14:33 +0700

    fix:  error

--- Phone-Frontend/src/utils/imageUtils.js
+++ Phone-Frontend/src/utils/imageUtils.js
@@ -48,7 +48,9 @@ export const getFallbackImage = () => {
   return 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100"%3E%3Crect fill="%23e5e7eb" width="100" height="100"/%3E%3Ctext x="50" y="50" dominant-baseline="middle" text-anchor="middle" font-family="Arial" font-size="12" fill="%239ca3af"%3ENo Image%3C/text%3E%3C/svg%3E';
 };
 
-export default {
+const imageUtils = {
   getImageUrl,
   getFallbackImage,
 };
+
+export default imageUtils;
